e350d988ec103af2d200cc43956dda8c9e78977f
Akshay Damle <akshaydamle95@gmail.com>
2015-04-24 22:03:25 +0530

    Set closeOnClick to false so the side-nav doesn't close when a link is clicked

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$('.button-collapse').sideNav({
 		menuWidth: 250, // Default is 240
 		edge: 'left', // Choose the horizontal origin
-		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 	
 	$('.slider').slider({full_width: true});
