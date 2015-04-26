a41aec699dc6c21947174ed2aecedaf590bd1b6b
Rajat Agarwal <rajat503@gmail.com>
2015-04-26 15:11:13 +0530

    updated email content

--- confirm.php
+++ confirm.php
@@ -1,6 +1,5 @@
 <?php
-if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['designation']) && isset($_POST['organization']) && isset($_POST['address']) && isset($_POST['pincode']) && isset($_POST['city']) && isset($_POST['telephone']) && isset($_POST['mobile']) && isset($_POST['email']) && isset($_POST['acco']) && isset($_POST['oral_poster']) && isset($_POST['acco_fee']) && isset($_POST['reg_fee']) && isset($_POST['total']) && isset($_POST['trans_id']))
-{
+
 	$first_name=$_POST['first_name'];
 	$last_name=$_POST['last_name'];
 	$designation=$_POST['designation'];
@@ -13,13 +12,16 @@ if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['de
 	$email=$_POST['email'];
 	$acco=$_POST['acco'];
 	$oral_poster=$_POST['oral_poster'];
+	$abstract=$_POST['abstract'];
+	$accompany=$_POST['accompany'];
 	$acco_fee=$_POST['acco_fee'];
 	$reg_fee=$_POST['reg_fee'];
 	$total=$_POST['total'];
+	$bank=$_POST['bank'];
 	$trans_id=$_POST['trans_id'];	
+	$date_trans=$_POST['date_trans'];
 
-
-	if(!empty($first_name) && !empty($first_name) && !empty($last_name) && !empty($designation) && !empty($organization) && !empty($address) && !empty($pincode) && !empty($city) && !empty($telephone) && !empty($mobile) && !empty($email) && !empty($acco) && !empty($oral_poster) && !empty($acco_fee) && !empty($reg_fee) && !empty($total) && !empty($trans_id))
+	if(!empty($first_name) && !empty($last_name) && !empty($designation) && !empty($organization) && !empty($address) && !empty($pincode) && !empty($city) && !empty($mobile) && !empty($email) && !empty($acco) && !empty($abstract) && !empty($reg_fee) && !empty($total) && !empty($trans_id) && !empty($bank) && !empty($date_trans))
 	{
 		require 'PHPMailer-master/PHPMailerAutoload.php';
 
@@ -30,7 +32,7 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // SMTP username
+$mail->Username = 'nfcfa2015@gmail.com';                 // SMTP username
 $mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
@@ -47,10 +49,10 @@ $mail->addCC('mainak@goa.bits-pilani.ac.in');
 // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'New Registration for NFCFA';
+$mail->Subject = 'Registration for NFCFA 2015';
 $mail->Body    = '<body>
-<p7><strong>Details</strong></p7></div>
-<p><strong>Name: </strong>'.$first_name.' '.$last_name.'<br>'.'<strong>Designation: </strong>'.$designation.'<br>'.'<strong>Organization: </strong>'.$organization.'<br>'.'<strong>Address: </strong>'.$address.'<br>'.'<strong>Pincode: </strong>'.$pincode.'<br>'.'<strong>City: </strong>'.$city.'<br>'.'<strong>Telephone: </strong>'.$telephone.'<br>'.'<strong>Mobile: </strong>'.$mobile.'<br>'.'<strong>E-mail: </strong>'.$email.'<br>'.'<strong>Accomodation: </strong>'.$acco.'<br>'.'<strong>Type: </strong>'.$oral_poster.'<br>'.'<strong>Accomodation Fee: </strong>'.$acco_fee.'<br>'.'<strong>Registration Fee: </strong>'.$reg_fee.'<br>'.'<strong>Total: </strong>'.$total.'<br>'.'<strong>Transaction ID: </strong>'.$trans_id.'<br>'.'</p></body>'; 
+<p7><strong>Registration Details:</strong></p7></div>
+<p><br><strong>Name: </strong>'.$first_name.' '.$last_name.'<br>'.'<strong>Designation: </strong>'.$designation.'<br>'.'<strong>Organization: </strong>'.$organization.'<br>'.'<strong>Address: </strong>'.$address.'<br>'.'<strong>Pincode: </strong>'.$pincode.'<br>'.'<strong>City: </strong>'.$city.'<br>'.'<strong>Telephone: </strong>'.$telephone.'<br>'.'<strong>Mobile: </strong>'.$mobile.'<br>'.'<strong>E-mail: </strong>'.$email.'<br>'.'<strong>Accomodation: </strong>'.$acco.'<br>'.'<strong>Type: </strong>'.$oral_poster.'<br>'.'<strong>Abstract Enclosed: </strong>'.$abstract.'<br>'.'<strong>Accompanying Person: </strong>'.$accompany.'<br>'.'<strong>Accommodation Fee: </strong>'.$acco_fee.'<br>'.'<strong>Registration Fee: </strong>'.$reg_fee.'<br>'.'<strong>Total: </strong>'.$total.'<br>'.'<strong>Bank: </strong>'.$bank.'<br>'.'<strong>Transaction ID: </strong>'.$trans_id.'<br>'.'<strong>Date: </strong>'.$date_trans.'<br> <br>'.'<strong>Signature: </strong>'.'</p><p>Please send a duly signed scanned or hard copy of this email to Convener, NFCFA 2015 at mainak@goa.bits-pilani.ac.in in case of NEFT transfers or by post if you are using a demand draft for payment.</p></body>'; 
 
 if(!$mail->send()) {
     header('Location: fail.html');
@@ -63,9 +65,4 @@ if(!$mail->send()) {
 	{
 		header('Location: incomplete.html');
 	}
-}
-else
-{
-	header('Location: incomplete.html');
-}
 ?>
